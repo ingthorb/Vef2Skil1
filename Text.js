c69646c798397314df6678083080cb44f7def435
ingthorb@gmail.com <ingthorb@gmail.com>
2016-01-25 16:51:41 +0000

    Deleted comments

--- Text.js
+++ Text.js
@@ -2,15 +2,9 @@ var Textt = Shape.extend({
 
 	constructor: function() {
 		this.base("Textt");
-		//var bla = "ello world!":
 	},
 
 	draw: function(canvas) {
-		//console.log(this.tFont);
-		//console.log(this.pText);
-		//Taka inn font size
-		//svo fontið sjálft
-
 		canvas.lineWidth = this.lineWidth;
 		var fontoption = String(this.tFont);
 		var fsize = String(this.fSize);
@@ -20,38 +14,9 @@ var Textt = Shape.extend({
 			console.log("Testing success");
 		}
 		var sz = fsize + "px " + fontoption;
-			/*var fontoption = String(this.tFont);
-		console.log(fontoption);
-		var fsize = String(this.fSize);
-		console.log(fsize);
-		var sz = String(fsize + " " +fontoption);*/
-		//sz.concat(fontoption);
+
 		canvas.font = sz;
 		console.log(sz);
-		/*switch(fontoption){
-			case 'Arial':
-				canvas.font = "20px Arial";
-				break;
-			case 'Comic Sans':
-				canvas.font = "20px Comic Sans";
-				break;
-			case 'Georgia':
-				canvas.font = "20px Georgia";
-				break;
-			case 'Tahoma':
-				canvas.font = "20px Tahoma";
-				break;
-			case 'Impact':
-				canvas.font = "20px Impact";
-				break;
-			case 'Courier New':
-				canvas.font = "20px Courier New";
-			default:
-				canvas.font = "20px Arial";
-
-		}*/
-		//canvas.font = "20px Arial"
-		//console.log(this.tFont);
 		canvas.strokeStyle = this.color;
 		canvas.strokeText(this.pText,this.pos.x,this.pos.y);
 
