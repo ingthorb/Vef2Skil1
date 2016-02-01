fb06cc1d473f7f37409575cabf38808e9add1ad2
ingthorb@gmail.com <ingthorb@gmail.com>
2016-02-01 19:50:52 +0000

    Fixed spray color

--- spray.js
+++ spray.js
@@ -8,15 +8,15 @@ var Spray = Shape.extend({
 	draw: function(canvas)
 	{
 		var particles = 40;
-		canvas.strokeStyle = this.color;
+		canvas.fillStyle = this.color;
 		canvas.lineWidth = this.lineWidth;
 		canvas.beginPath();
-		for(var i = 0; i < particles; i++)	
-		{
-			var off = this.offset(10);
-			var x_new = this.pos.x + off[0];
-			var y_new = this.pos.y + off[1];
-
+		 for (var i = particles; i--; ) 
+		 {
+			var angle = this.offset(0,2*Math.PI);
+			var radius = this.offset(0,15);
+			var x_new = this.pos.x + radius * Math.cos(angle);
+			var y_new = this.pos.y + radius * Math.sin(angle);
 			canvas.fillRect(x_new,y_new,2,2);
 			canvas.stroke();
 		}
@@ -26,16 +26,10 @@ var Spray = Shape.extend({
 	{
 
 	},
-	offset: function(radius)
+	offset: function(min,max)
 	{
-		var arr = new Array();
-		var randomangle = Math.random() * (2*Math.PI);
-		var randomradius = Math.random() * radius;
-		//x
-		arr.push(Math.cos(randomangle) * randomradius);
-		//y
-		 arr.push(Math.sin(randomangle) * randomradius);
-		return arr;
+		var ret = Math.random() * (max-min) + min;
+		return ret;
 	}
 
 });
