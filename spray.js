44c5e3a272e75266e9ba556225ffa77c3e3ab80a
LogiGudmann <logi@gudmann.is>
2016-02-01 20:38:38 +0000

    fixed spray

--- spray.js
+++ spray.js
@@ -2,34 +2,39 @@ var Spray = Shape.extend({
 
 	constructor: function()
 	{
+		this.sprayArrX = new Array();
+		this.sprayArrY = new Array();
 		this.base("Spray");
 	},
 
 	draw: function(canvas)
 	{
-		var particles = 40;
 		canvas.fillStyle = this.color;
 		canvas.lineWidth = this.lineWidth;
 		canvas.beginPath();
-		 for (var i = particles; i--; ) 
-		 {
-			var angle = this.offset(0,2*Math.PI);
-			var radius = this.offset(0,15);
-			var x_new = this.pos.x + radius * Math.cos(angle);
-			var y_new = this.pos.y + radius * Math.sin(angle);
-			canvas.fillRect(x_new,y_new,2,2);
-			canvas.stroke();
+		canvas.moveTo(this.pos.x, this.pos.y);
+		for(var i = 0; i < this.sprayArrX.length; i++){
+			canvas.fillRect(this.sprayArrX[i], this.sprayArrY[i],2,2);
 		}
+		canvas.stroke();
 		this.base(canvas);
 	},
+
 	drawing:function(point)
 	{
-
+		var particles = 40;
+	 	for (var i = 0; i < particles; i++){
+			var angle = this.offset(0,2*Math.PI);
+			var radius = this.offset(0,15);
+			var x_new = radius * Math.cos(angle);
+			var y_new = radius * Math.sin(angle);
+			this.sprayArrX.push(point.x + x_new);
+			this.sprayArrY.push(point.y + y_new);
+		}
 	},
+
 	offset: function(min,max)
 	{
-		var ret = Math.random() * (max-min) + min;
-		return ret;
+		return Math.random() * (max-min) + min;
 	}
-
 });
